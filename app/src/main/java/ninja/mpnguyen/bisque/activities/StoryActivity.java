5aceb1d35267dd5812b3b8cc0e122f582399aa0d
Mark Nguyen <mpnguyen@umail.ucsb.edu>
2015-06-04 13:43:52 -0700

    Stories now refresh when you pull down

--- app/src/main/java/ninja/mpnguyen/bisque/activities/StoryActivity.java
+++ app/src/main/java/ninja/mpnguyen/bisque/activities/StoryActivity.java
@@ -9,8 +9,8 @@ import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
-import android.view.View;
 
+import java.lang.ref.WeakReference;
 import java.util.List;
 
 import ninja.mpnguyen.bisque.R;
@@ -20,7 +20,7 @@ import ninja.mpnguyen.bisque.views.comments.StoryAdapter;
 import ninja.mpnguyen.chowders.things.Post;
 import ninja.mpnguyen.chowders.things.Story;
 
-public class StoryActivity extends AppCompatActivity {
+public class StoryActivity extends AppCompatActivity implements SwipeRefreshLayout.OnRefreshListener {
     public static final String EXTRA_POST = "ninja.mpnguyen.bisque.activities.StoryActivity.EXTRA_POST";
 
     @Override
@@ -30,6 +30,7 @@ public class StoryActivity extends AppCompatActivity {
 
         SwipeRefreshLayout swipeRefreshLayout = (SwipeRefreshLayout) findViewById(R.id.swipe);
         swipeRefreshLayout.setColorSchemeColors(Color.RED, Color.BLUE, Color.GREEN);
+        swipeRefreshLayout.setOnRefreshListener(this);
 
         RecyclerView recyclerView = (RecyclerView) findViewById(R.id.content_view);
         final LinearLayoutManager layoutManager = new LinearLayoutManager(this);
@@ -39,13 +40,10 @@ public class StoryActivity extends AppCompatActivity {
         Intent intent = getIntent();
         Post post = intent.hasExtra(EXTRA_POST) ? (Post) intent.getSerializableExtra(EXTRA_POST) : null;
         if (post != null) {
-            recyclerView.setVisibility(View.VISIBLE);
             recyclerView.swapAdapter(new StoryAdapter(new Story(post)), false);
         }
 
-        String short_id = getShortId(intent.getData());
-        StoryFetchedListener listener = new StoryFetchedListener(swipeRefreshLayout, recyclerView);
-        new StoryFetcherTask(listener, short_id).execute();
+        onRefresh();
     }
 
     private String getShortId(Uri uri) {
@@ -57,28 +55,53 @@ public class StoryActivity extends AppCompatActivity {
         }
     }
 
+    @Override
+    public void onRefresh() {
+        SwipeRefreshLayout swipeRefreshLayout = (SwipeRefreshLayout) findViewById(R.id.swipe);
+        if (swipeRefreshLayout == null) return;
+
+        RecyclerView recyclerView = (RecyclerView) findViewById(R.id.content_view);
+        if (recyclerView == null) return;
+
+        Intent intent = getIntent();
+        String short_id = getShortId(intent.getData());
+        StoryFetchedListener listener = new StoryFetchedListener(swipeRefreshLayout, recyclerView);
+        new StoryFetcherTask(listener, short_id).execute();
+    }
+
     private static class StoryFetchedListener implements FetcherTask.Listener<Story> {
-        private final SwipeRefreshLayout refreshLayout;
-        private final RecyclerView content;
+        private final WeakReference<SwipeRefreshLayout> refreshLayoutRef;
+        private final WeakReference<RecyclerView> contentRef;
 
         private StoryFetchedListener(SwipeRefreshLayout refreshLayout, RecyclerView content) {
-            this.refreshLayout = refreshLayout;
-            this.content = content;
+            this.refreshLayoutRef = new WeakReference<>(refreshLayout);
+            this.contentRef = new WeakReference<>(content);
         }
 
         @Override
         public void onStart() {
+            SwipeRefreshLayout refreshLayout = refreshLayoutRef.get();
+            if (refreshLayout == null) return;
+
             refreshLayout.setRefreshing(true);
         }
 
         @Override
         public void onSuccess(@NonNull Story story) {
+            SwipeRefreshLayout refreshLayout = refreshLayoutRef.get();
+            RecyclerView content = contentRef.get();
+            if (refreshLayout == null || content == null) return;
+
             refreshLayout.setRefreshing(false);
             content.swapAdapter(new StoryAdapter(story), false);
         }
 
         @Override
         public void onError() {
+            SwipeRefreshLayout refreshLayout = refreshLayoutRef.get();
+            RecyclerView content = contentRef.get();
+            if (refreshLayout == null || content == null) return;
+
             refreshLayout.setRefreshing(false);
             content.swapAdapter(new StoryAdapter(null), false);
         }
