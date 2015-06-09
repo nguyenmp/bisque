590c294bf45d93a7bb12338b0351b4ec718271d7
Mark Nguyen <mpnguyen@umail.ucsb.edu>
2015-06-09 12:27:13 -0700

    Fixed weird issue where editor would be null and cause us to either crash or erroniously show could not load posts

--- app/src/main/java/ninja/mpnguyen/bisque/loaders/PostsFromServerLoader.java
+++ app/src/main/java/ninja/mpnguyen/bisque/loaders/PostsFromServerLoader.java
@@ -1,6 +1,7 @@
 package ninja.mpnguyen.bisque.loaders;
 
 import android.content.Context;
+import android.util.Log;
 
 import java.io.IOException;
 
@@ -15,9 +16,9 @@ public class PostsFromServerLoader extends GenericLoader<Post[]> {
 
     @Override
     public Post[] loadInBackground() {
+        Post[] posts = null;
         try {
-            Post[] posts = FrontPage.get(FrontPage.Sort.Hottest);
-            if (posts != null) new PostsCashier().putIntoCache(posts);
+            posts = FrontPage.get(FrontPage.Sort.Hottest);
             return posts;
         } catch (IOException e) {
             e.printStackTrace();
@@ -25,6 +26,16 @@ public class PostsFromServerLoader extends GenericLoader<Post[]> {
         } catch (Exception e) {
             e.printStackTrace();
             return null;
+        } finally {
+            try {
+                // When returning from a killed state, this action of
+                // putting it into the cache can throw a null pointer
+                // this would cause us to return null in the above block
+                // which is invalid because posts could have been non-null
+                if (posts != null) new PostsCashier().putIntoCache(posts);
+            } catch (Exception e) {
+                Log.i(PostsFromServerLoader.class.getName(), "Failed to put " + posts + " into the cache.", e);
+            }
         }
     }
 }
