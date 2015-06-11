5e05ce769962d5a0d063727c86037797a79e7634
Mark Nguyen <mpnguyen@umail.ucsb.edu>
2015-06-11 04:28:30 -0700

    Implemented code handling in comments

--- app/src/main/java/ninja/mpnguyen/bisque/views/comments/CommentPresenter.java
+++ app/src/main/java/ninja/mpnguyen/bisque/views/comments/CommentPresenter.java
@@ -1,9 +1,18 @@
 package ninja.mpnguyen.bisque.views.comments;
 
+import android.text.Editable;
 import android.text.Html;
+import android.text.TextUtils;
+import android.text.style.TypefaceSpan;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+import org.xml.sax.XMLReader;
 
 import ninja.mpnguyen.bisque.R;
 import ninja.mpnguyen.chowders.things.json.Comment;
@@ -15,11 +24,38 @@ public class CommentPresenter {
     }
 
     public static void bindListItem(CommentViewHolder holder, Comment comment) {
-        holder.comment_text.setText(Html.fromHtml(comment.comment));
+        String comment1 = comment.comment;
+        Document dom = Jsoup.parse(comment1);
+        Elements codes = dom.select("code");
+        for (Element e : codes) {
+            String encoded = TextUtils.htmlEncode(e.text().replaceAll(" ", "\u00A0"));
+            encoded = encoded.replaceAll("\n", "<br>");
+            e.html(encoded);
+        }
+        holder.comment_text.setText(Html.fromHtml(dom.body().html(), null, new CodeHandler()));
         holder.comment_author.setText(comment.commenting_user.username);
 
         ViewGroup.LayoutParams layoutParams = holder.padding.getLayoutParams();
         layoutParams.width = comment.indent_level * 40;
         holder.padding.setLayoutParams(layoutParams);
+    }
+
+    private static class CodeHandler implements Html.TagHandler {
+        private Integer start;
+        @Override
+        public void handleTag(boolean opening, String tag, Editable output, XMLReader xmlReader) {
+            if (tag.equalsIgnoreCase("code")) {
+                if (opening) {
+                    if (start == null) start = output.length();
+                }
+
+                if (!opening && start != null) {
+                    int end = output.length();
+                    int flags = 0;
+                    output.setSpan(new TypefaceSpan("monospace"), start, end, flags);
+                    start = null;
+                }
+            }
+        }
     }
 }
