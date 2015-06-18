cf92fbff24cd1a6730a550d865b1b9791c8a89ce
Mark Nguyen <mpnguyen@umail.ucsb.edu>
2015-06-18 00:46:11 -0700

    Added google analytics tracking

--- app/src/main/java/ninja/mpnguyen/bisque/MyApplication.java
+++ app/src/main/java/ninja/mpnguyen/bisque/MyApplication.java
@@ -3,8 +3,13 @@ package ninja.mpnguyen.bisque;
 import android.app.Application;
 
 import com.anupcowkur.reservoir.Reservoir;
+import com.google.android.gms.analytics.GoogleAnalytics;
+import com.google.android.gms.analytics.Tracker;
 
 public class MyApplication extends Application {
+    public static GoogleAnalytics analytics;
+    public static Tracker tracker;
+
     @Override
     public void onCreate() {
         super.onCreate();
@@ -13,5 +18,12 @@ public class MyApplication extends Application {
         } catch (Exception e) {
             e.printStackTrace();
         }
+
+        analytics = GoogleAnalytics.getInstance(this);
+        analytics.setLocalDispatchPeriod(1800);
+
+        tracker = analytics.newTracker("UA-64250762-1"); // Replace with actual tracker/property Id
+        tracker.enableExceptionReporting(true);
+        tracker.enableAutoActivityTracking(true);
     }
 }
